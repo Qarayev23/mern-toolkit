1c039353be9a2df5a849bb31e2b38199054fd9eb
Nicat <qarayevn61@gmail.com>
2022-03-16 18:40:04 +0400

    update

---
+++ server/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import morgan from "morgan";
+import userRouter from "./routes/user.js";
+import tourRouter from "./routes/tour.js";
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use("/users", userRouter); // http://localhost:8000/users/signup
+app.use("/tour", tourRouter);
+
+const MONGODB_URL = "mongodb+srv://NIcat:vugNGAhtB5jP2IhT@cluster0.grjig.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const port = 8000;
+
+mongoose
+  .connect(MONGODB_URL)
+  .then(() => {
+    app.listen(port, () => console.log(`Server running on port ${port}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
